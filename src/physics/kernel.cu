5b89beac9061aedca465241e820c3f35dd92216e
Anthony Mansur <amansur99@gmail.com>
2021-11-09 21:46:21 -0500

    init project template

---
+++ src/physics/kernel.cu
@@ -0,0 +1,35 @@
+#include <cuda.h>
+
+#include "kernel.h"
+
+/*****************
+* Configuration *
+*****************/
+
+/***********************************************
+* Kernel state (pointers are device pointers) *
+***********************************************/
+
+/******************
+* initSimulation *
+******************/
+void Simulation::initSimulation(int N)
+{
+    // TODO: implement
+}
+
+/******************
+* stepSimulation *
+******************/
+void Simulation::stepSimulation(float dt)
+{
+    // TODO: implement
+}
+
+/******************
+* endSimulation *
+******************/
+void Simulation::endSimulation()
+{
+    // TODO: implement
+}
