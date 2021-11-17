913438655c35d9fd8cda3028100b0515d5b19288
Lindsay Smith <lsmith24@seas.upenn.edu>
2021-11-17 15:27:15 -0500

    shaders

--- shaders/graphics.frag.glsl
+++ shaders/graphics.frag.glsl
@@ -1,15 +1,13 @@
 #version 330
 
 in vec2 texCoord;
-in vec4 color;
+in vec3 color;
 
 out vec4 outColor;
 
 uniform sampler2D ourTex;
 
 void main() {
-	outColor = texture(ourTex, texCoord);
-	/*outColor.r = vFragColor.r;
-	outColor.g = vFragColor.g;
-	outColor.b = vFragColor.b;*/
+	//outColor = texture(ourTex, texCoord);
+	outColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
--- shaders/graphics.vert.glsl
+++ shaders/graphics.vert.glsl
@@ -10,13 +10,13 @@ void main() {
 
 #version 330
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec4 aColor;
+layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTexCoord;
 
 uniform mat4 u_projMatrix;
 
 out vec2 texCoord;
-out vec4 color;
+out vec3 color;
 
 void main() {
 	gl_Position = u_projMatrix * vec4(aPos, 1.0);
