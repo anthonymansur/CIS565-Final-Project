b1a3e4ae22d68d56cfeb35aff77c7509e4ee8e4a
Stephen Lee <slee11030@users.noreply.github.com>
2021-11-24 11:48:11 -0500

    Add grid params and computations

--- src/physics/kernel.cu
+++ src/physics/kernel.cu
@@ -18,8 +18,24 @@ dim3 threadsPerBlock(blockSize);
 Node* dev_nodes;
 Edge* dev_edges;
 Module* dev_modules;
-float3* dev_temp;
-float3* dev_old_temp;
+
+// Grid Dimensions
+int3 gridCount = { 20, 20, 20 };
+float3 gridSize = { 20.f, 20.f, 20.f };
+float sideLength = 1.f; // "blockSize"
+
+// TODO add rest of grid params
+float* dev_temp;
+float* dev_oldtemp;
+float3* dev_vel;
+float3* dev_oldvel;
+float* dev_pressure;
+float3* dev_ccvel;
+float3* dev_vorticity;
+float* dev_smokedensity;
+float* dev_oldsmokedensity;
+float* dev_deltaM;
+
 
 /******************
 * initSimulation *
@@ -28,6 +44,7 @@ float3* dev_old_temp;
 void Simulation::initSimulation(Terrain* terrain)
 {
     numOfModules = terrain->modules.size();
+    int numOfGrids = gridCount.x * gridCount.y * gridCount.z;
     dim3 fullBlocksPerGrid((numOfModules + blockSize - 1) / blockSize);
 
     // Allocate buffers for the modules
@@ -40,8 +57,37 @@ void Simulation::initSimulation(Terrain* terrain)
     cudaMalloc((void**)&dev_modules, terrain->modules.size() * sizeof(Module));
     cudaMemcpy(dev_modules, terrain->modules.data(), terrain->modules.size(), cudaMemcpyHostToDevice);
 
+    // TODO allocate grid buffers
+    cudaMalloc((void**)&dev_temp, numOfGrids * sizeof(float));
+    cudaMemset(dev_temp, T_AMBIANT, numOfGrids * sizeof(float));
 
+    cudaMalloc((void**)&dev_oldtemp, numOfGrids * sizeof(float));
+    cudaMemset(dev_oldtemp, 0, numOfGrids * sizeof(float));
 
+    cudaMalloc((void**)&dev_vel, numOfGrids * sizeof(float3));
+    cudaMemset(dev_vel, 0, numOfGrids * sizeof(float3));
+
+    cudaMalloc((void**)&dev_oldvel, numOfGrids * sizeof(float3));
+    cudaMemset(dev_oldvel, 0, numOfGrids * sizeof(float3));
+
+    cudaMalloc((void**)&dev_pressure, numOfGrids * sizeof(float));
+    cudaMemset(dev_pressure, 0, numOfGrids * sizeof(float));
+
+    cudaMalloc((void**)&dev_ccvel, numOfGrids * sizeof(float3));
+    cudaMemset(dev_ccvel, 0, numOfGrids * sizeof(float3));
+
+    cudaMalloc((void**)&dev_vorticity, numOfGrids * sizeof(float3));
+    cudaMemset(dev_vorticity, 0, numOfGrids * sizeof(float3));
+
+    cudaMalloc((void**)&dev_smokedensity, numOfGrids * sizeof(float));
+    cudaMemset(dev_smokedensity, 0, numOfGrids * sizeof(float));
+
+    cudaMalloc((void**)&dev_oldsmokedensity, numOfGrids * sizeof(float));
+    cudaMemset(dev_oldsmokedensity, 0, numOfGrids * sizeof(float));
+
+    cudaMalloc((void**)&dev_deltaM, numOfGrids * sizeof(float));
+    cudaMemset(dev_deltaM, 0, numOfGrids * sizeof(float));
+        
     // TODO: check cuda error
 }
 
@@ -79,7 +125,36 @@ void Simulation::stepSimulation(float dt)
     // update drag forces (wind)
     // update smoke density (qs), water vapor (qv), condensed water (qc),
     // and rain (qc)
-    // TODO: implement
+    float* dev_lap;
+    float3* dev_alpha_m;
+    float3 externalForce = { 0.f, 0.f, 0.f };
+
+    const dim3 M_in(M_IX, M_IY, M_IZ);
+    const dim3 gridDim(blocksNeeded(gridCount.x, M_IX), blocksNeeded(gridCount.y, M_IY), blocksNeeded(gridCount.z, M_IZ));
+
+    HANDLE_ERROR(cudaMalloc(&dev_lap, gridCount.x * gridCount.y * gridCount.z * sizeof(float)));
+    HANDLE_ERROR(cudaMalloc(&dev_alpha_m, gridCount.x * gridCount.y * gridCount.z * sizeof(float3)));
+
+    computeVorticity << <gridDim, M_in >> > (gridCount, blockSize, dev_vorticity, dev_oldvel, dev_ccvel);
+    HANDLE_ERROR(cudaPeekAtLastError()); HANDLE_ERROR(cudaDeviceSynchronize());
+    velocityKernel << <gridDim, M_in >> > (gridCount, gridSize, blockSize, dev_oldtemp, dev_vel, dev_oldvel, dev_alpha_m,
+        dev_oldsmokedensity, dev_vorticity, externalForce);
+    HANDLE_ERROR(cudaPeekAtLastError()); HANDLE_ERROR(cudaDeviceSynchronize());
+
+    // Pressure Solve
+    forceIncompressibility(gridCount, blockSize, dev_vel, dev_pressure);
+
+    tempAdvectionKernel << <gridDim, M_in >> > (gridCount, gridSize, blockSize, dev_temp, dev_oldtemp, dev_vel, dev_alpha_m, dev_lap);
+    HANDLE_ERROR(cudaPeekAtLastError()); HANDLE_ERROR(cudaDeviceSynchronize());
+
+    smokeUpdateKernel << <gridDim, M_in >> > (gridCount, gridSize, blockSize, dev_oldtemp, dev_vel, dev_alpha_m, dev_smokedensity, 
+        dev_oldsmokedensity, dev_deltaM);
+
+    HANDLE_ERROR(cudaPeekAtLastError());
+    HANDLE_ERROR(cudaDeviceSynchronize());
+
+    HANDLE_ERROR(cudaFree(dev_alpha_m));
+    HANDLE_ERROR(cudaFree(dev_lap));
 
     // For each module in the forest
     // cull modules (and their children) that have zero mass
